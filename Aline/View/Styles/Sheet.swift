50b877e2b8cb67c6efb73d8782ca2f5b7a6d2d72
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-08-07 19:14:32 -0600

    Sheet set it in a new file

---
+++ Aline/View/Styles/Sheet.swift
@@ -0,0 +1,18 @@
+//
+//  Sheet.swift
+//  Aline
+//
+//  Created by Leonardo on 07/08/23.
+//
+
+import SwiftUI
+
+struct Sheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sheet()
+}
