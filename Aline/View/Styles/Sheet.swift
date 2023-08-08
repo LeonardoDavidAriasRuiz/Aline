ec38717c1949cd0ca47a9b7f9c43578b1a05ef1d
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-08-07 19:15:12 -0600

    Sheet set it in a new file

--- Aline/View/Styles/Sheet.swift
+++ Aline/View/Styles/Sheet.swift
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
-struct Sheet: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Sheet<Content: View>: View {
+     let title: String
+     let content: () -> Content
+    
+    init(title: String, @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.title = title
     }
-}
-
-#Preview {
-    Sheet()
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack(content: content)
+                    .frame(maxWidth: 800)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
+        }
+        .navigationTitle(title)
+        .navigationBarTitleDisplayMode(.large)
+    }
 }
--- Aline/View/Styles/Styles.swift
+++ Aline/View/Styles/Styles.swift
@@ -167,30 +167,7 @@ struct LoadingIfNotReady<Content: View>: View  {
     }
 }
 
-struct Sheet<Content: View>: View {
-     let title: String
-     let content: () -> Content
-    
-    init(title: String, @ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-        self.title = title
-    }
-    
-    var body: some View {
-        VStack {
-            ScrollView {
-                VStack(content: content)
-                    .frame(maxWidth: 800)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.background)
-        }
-        .navigationTitle(title)
-        .navigationBarTitleDisplayMode(.large)
-    }
-}
+
 
 struct WhiteArea<Content: View>: View {
      let content: () -> Content
