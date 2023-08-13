fa59d5685cbb774e8597dfd5278749cbdfae1588
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-08-13 17:07:10 -0600

    Creation of its file for Editable Name

---
+++ Aline/View/Styles/EditableName.swift
@@ -0,0 +1,60 @@
+//
+//  EditableName.swift
+//  Aline
+//
+//  Created by Leonardo on 13/08/23.
+//
+
+import SwiftUI
+
+struct EditableName: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @Binding var name: String
+    @State private var newName: String = ""
+    @State private var updateButtonDisabled: Bool = true
+    let accion: () -> Void
+    var body: some View {
+        Sheet(title: "Nombre") {
+            WhiteArea {
+                TextField("Nombre", text: $newName)
+                    .textInputAutocapitalization(.words)
+                    .keyboardType(.alphabet)
+                    .autocorrectionDisabled(true)
+                    .onAppear(perform: onApper)
+                    .onChange(of: newName, isValidName)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Actualizar", action: update)
+                        .disabled(updateButtonDisabled)
+                }
+            }
+        }
+    }
+    
+    private func onApper() {
+        newName = name
+    }
+    
+    private func isValidName() {
+        if name != newName {
+            if newName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                updateButtonDisabled = true
+                return
+            } else {
+                let nameRegex = "^[a-zA-Z ]+$"
+                let namePredicate = NSPredicate(format: "SELF MATCHES %@", nameRegex)
+                updateButtonDisabled = !namePredicate.evaluate(with: newName)
+            }
+        } else {
+            updateButtonDisabled = true
+        }
+    }
+    
+    private func update() {
+        self.presentationMode.wrappedValue.dismiss()
+        name = newName
+        accion()
+    }
+}
