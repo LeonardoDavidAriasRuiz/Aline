e76110db8862b4247012de856eb8da181360d3c1
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-08-13 17:00:48 -0600

    Colors extansion change

---
+++ Aline/View/Styles/AlineColors.swift
@@ -0,0 +1,16 @@
+//
+//  AlineColors.swift
+//  Aline
+//
+//  Created by Leonardo on 13/08/23.
+//
+
+import SwiftUI
+
+extension Color {
+     static var green: Color = Color("Green")
+     static var blue: Color = Color("Blue")
+     static var red: Color = Color("Red")
+     static var orange: Color = Color("Orange")
+     static var background: Color = Color("Background")
+}
