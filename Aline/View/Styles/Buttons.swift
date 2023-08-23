61dae9e84ecbbdedf3180e2c1ae4698cab3fbbde
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-08-23 10:17:13 -0600

    Views for action buttons created

--- Aline/View/Styles/Buttons.swift
+++ Aline/View/Styles/Buttons.swift
@@ -7,12 +7,121 @@
 
 import SwiftUI
 
-struct Buttons: View {
+struct NewButton: View {
+    @Binding var pressed: Bool
+    
+    let newText: String
+    let action: () -> Void
+    
+    private let cancelButtonText: String = "Cancelar"
+    private let buttonSymbol: String = "plus"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            HStack {
+                Text(pressed ? cancelButtonText : newText)
+                Spacer()
+                Image(systemName: buttonSymbol)
+                    .font(.title2)
+                    .rotationEffect(Angle(degrees: pressed ? 45 : 0))
+                    .symbolEffect(.bounce, value: pressed)
+            }
+        }
     }
 }
 
-#Preview {
-    Buttons()
+struct SaveButton: View {
+    let action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            Text("Guardar").frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct CancelButton: View {
+    let action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            Text("Cancelar").frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct UpdateButton: View {
+    let action: () -> Void
+    var body: some View {
+        Button(action: action) {
+            Text("Actualizar").frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct DeleteButton: View {
+    @State private var deleteAlertShowed: Bool = false
+    
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: showDeleteAlert) {
+            Text("Eliminar").frame(maxWidth: .infinity)
+        }
+        .alertDelete(showed: $deleteAlertShowed, action: action)
+    }
+    
+    private func showDeleteAlert() {
+        deleteAlertShowed = true
+    }
+}
+
+struct SaveButtonWhite: View {
+    let action: () -> Void
+    var body: some View {
+        WhiteArea {
+            Button(action: action) {
+                Text("Guardar").frame(maxWidth: .infinity)
+            }
+        }
+    }
+}
+
+struct CancelButtonWhite: View {
+    let action: () -> Void
+    var body: some View {
+        WhiteArea {
+            Button(action: action) {
+                Text("Cancelar").frame(maxWidth: .infinity)
+            }
+        }
+    }
+}
+
+struct UpdateButtonWhite: View {
+    let action: () -> Void
+    var body: some View {
+        WhiteArea {
+            Button(action: action) {
+                Text("Actualizar").frame(maxWidth: .infinity)
+            }
+        }
+    }
+}
+
+struct DeleteButtonWhite: View {
+    @State private var deleteAlertShowed: Bool = false
+    
+    let action: () -> Void
+    
+    var body: some View {
+        WhiteArea {
+            Button(action: showDeleteAlert) {
+                Text("Eliminar").frame(maxWidth: .infinity)
+            }
+            .alertDelete(showed: $deleteAlertShowed, action: action)
+        }
+    }
+    
+    private func showDeleteAlert() {
+        deleteAlertShowed = true
+    }
 }
