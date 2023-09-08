cc7a82975dc386963c49dfb409e52f7ccafdd26b
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-09-08 11:20:04 -0600

    Methods and variables added to AlertViewModel

--- Aline/ViewModels/AlertViewModel.swift
+++ Aline/ViewModels/AlertViewModel.swift
@@ -9,10 +9,29 @@ import Foundation
 
 class AlertViewModel: ObservableObject {
     @Published var alertType: AlertType = .dataObtainingError
-    @Published var alertShowed: Bool = false
+    @Published var alertInfoShowed: Bool = false
+    @Published var alertDeleteShowed: Bool = false
+    @Published var alertCancelShowed: Bool = false
+    @Published var alertDeclineShowed: Bool = false
+    @Published var action: () -> Void = {}
     
     func show(_ alertType: AlertType) {
         self.alertType = alertType
-        alertShowed = true
+        alertInfoShowed = true
+    }
+    
+    func delete(action: () -> Void ) {
+        self.action = action
+        alertDeleteShowed = true
+    }
+    
+    func cancel(action: () -> Void ) {
+        self.action = action
+        alertCancelShowed = true
+    }
+    
+    func decline(action: () -> Void ) {
+        self.action = action
+        alertDeclineShowed = true
     }
 }
