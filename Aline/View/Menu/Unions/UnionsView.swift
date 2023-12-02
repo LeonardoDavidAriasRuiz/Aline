4e106eb78a3e59f98c10a07868afbec13f3584a5
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-12-02 17:43:00 -0600

    Date pickers change

---
+++ Aline/View/Menu/Unions/UnionsView.swift
@@ -0,0 +1,18 @@
+//
+//  UnionsView.swift
+//  Aline
+//
+//  Created by Leonardo on 30/11/23.
+//
+
+import SwiftUI
+
+struct UnionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnionsView()
+}
---
+++ Aline/ViewModels/CheckViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CheckViewModel.swift
+//  Aline
+//
+//  Created by Leonardo on 27/11/23.
+//
+
+import Foundation
